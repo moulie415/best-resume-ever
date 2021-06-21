70d7df2f5704ecc5a78712cdd59ea15f6ff9dba5
Henry Moule <henry@care-planner.co.uk>
2021-06-21 15:39:37 +0100

    updated resume

--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -39,7 +39,7 @@ export default Vue.component('resume', {
   background: white;
   position: relative;
   width: 21cm;
-  height: 40.68cm;
+  height: 47.68cm;
   display: block;
   page-break-after: auto;
   overflow: hidden;
